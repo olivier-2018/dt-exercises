2b247767a607bde8238a7307975f7c5011d311fb
Vincenzo <polivicio@gmail.com>
2021-04-14 22:15:26 +0200

    odometry launcher

--- mooc-modcon/launchers/default-odometry.sh
+++ mooc-modcon/launchers/default-odometry.sh
@@ -17,7 +17,7 @@ dt-launchfile-init
 #python3 /code/solution.py &
 #dt-exec roslaunch --wait car_interface all.launch veh:=$VEHICLE_NAME &
 
-
+dt-exec roslaunch --wait car_interface all.launch veh:=$VEHICLE_NAME &
 dt-exec roslaunch encoder_pose encoder_pose_node.launch veh:=$VEHICLE_NAME
 
 
