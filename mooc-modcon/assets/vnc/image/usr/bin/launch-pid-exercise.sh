b367246c8548c91e1b9689ab20f991c15bf1c999
Vincenzo <polivicio@gmail.com>
2021-04-16 00:08:49 +0200

    change agent to robot name

--- mooc-modcon/assets/vnc/image/usr/bin/launch-pid-exercise.sh
+++ mooc-modcon/assets/vnc/image/usr/bin/launch-pid-exercise.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sed -i "s/agent/${VEHICLE_NAME}/g" /opt/ros/noetic/share/rviz/odometry.rviz
 rviz -d /opt/ros/noetic/share/rviz/odometry.rviz &
 rostopic pub /$VEHICLE_NAME/activity_name std_msgs/String "data: pid"
 #rostopic pub /$VEHICLE_NAME/joy_mapper_node/car_cmd duckietown_msgs/Twist2DStamped "{header: auto,  v: 0, omega: 0}"
