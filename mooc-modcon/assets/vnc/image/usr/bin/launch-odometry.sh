86bac555d8c8d1b9728f08f5efd45ed4b1880ba5
Vincenzo <polivicio@gmail.com>
2021-04-15 23:38:54 +0200

    topic name change dinamically

--- mooc-modcon/assets/vnc/image/usr/bin/launch-odometry.sh
+++ mooc-modcon/assets/vnc/image/usr/bin/launch-odometry.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sed "s/agent/${VEHICLE_NAME}/g" /opt/ros/noetic/share/rviz/odometry.rviz
 rostopic pub /$VEHICLE_NAME/activity_name std_msgs/String "data: odometry" &
 dt-launcher-joystick & 
 rviz -d /opt/ros/noetic/share/rviz/odometry.rviz
