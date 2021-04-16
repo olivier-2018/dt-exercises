7a1cb3383c5e41811198b70206a3b619978025ec
Vincenzo <polivicio@gmail.com>
2021-04-16 14:00:11 +0200

    AIDO submission set up

--- mooc-modcon/launchers/submit.sh
+++ mooc-modcon/launchers/submit.sh
@@ -7,7 +7,7 @@ source /code/catkin_ws/devel/setup.bash
 source /code/solution/devel/setup.bash
 python3 solution.py &
 roslaunch --wait car_interface all.launch veh:=$VEHICLE_NAME &
-roslaunch encoder_pose encoder_pose_node.launch veh:=$VEHICLE_NAME &
 sleep 5
+roslaunch encoder_pose encoder_pose_node.launch veh:=$VEHICLE_NAME AIDO_eval:="true" &
 rostopic pub /$VEHICLE_NAME/activity_name std_msgs/String "data: pid_exercise"
 
