ebb4a7e5d9a67a3db7bcb276232b32f889bd4094
tanij <tanij@ethz.ch>
2021-04-18 18:22:57 -0400

    PID control activity with new interactive pane description

--- mooc-modcon/solution/src/encoder_pose/src/PID_controller.py
+++ mooc-modcon/solution/src/encoder_pose/src/PID_controller.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[2]:
 
 
 import numpy as np
@@ -41,12 +41,10 @@ def PIDController(v_0, theta_ref, theta_hat, prev_e, prev_int, delta_t): #add th
     # PID controller for omega
     omega = Kp*e + Ki*e_int + Kd*e_der
     
-#     if abs(e - prev_e) < np.deg2rad(0.1):
-#         u = [0, 0]
-#     else:
     u = [v_0, omega]
     
-    print(f"\n\nDelta time : {delta_t} \nE : {np.rad2deg(e)} \nE int : {e_int} \nPrev e : {prev_e} \nU : {u} \nTheta hat: {np.rad2deg(theta_hat)} \n Abs e {np.rad2deg(abs(e - prev_e))}")
+    # For debugging
+    # print(f"\n\nDelta time : {delta_t} \nE : {np.rad2deg(e)} \nE int : {e_int} \nPrev e : {prev_e} \nU : {u} \nTheta hat: {np.rad2deg(theta_hat)} \n Abs e {np.rad2deg(abs(e - prev_e))}")
     
     return u, e, e_int
 
