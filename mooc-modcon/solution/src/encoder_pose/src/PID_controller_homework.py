5b91ac53ad0e87ec1919264744a9aecf185ccc73
Andrea F. Daniele <afdaniele@ttic.edu>
2021-04-21 13:59:19 -0500

    pre-release

--- mooc-modcon/solution/src/encoder_pose/src/PID_controller_homework.py
+++ mooc-modcon/solution/src/encoder_pose/src/PID_controller_homework.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[13]:
+# In[ ]:
 
 
 import numpy as np
@@ -28,41 +28,16 @@ def PIDController(
         prev_int_y (:double:) previous integral error term.
         delta_t (:double:) time interval since last call.
     returns:
-        u (:list:) 1x2 array of commands for the Duckiebot: [v0, omega] 
-        current_e (:double:) current tracking error (automatically becomes prev_e_y at next iteration).
-        current_int_e (:double:) current integral error (automatically becomes prev_int_y at next iteration).
+        v_0 (:double:) linear velocity of the Duckiebot 
+        omega (:double:) angular velocity of the Duckiebot
+        e_y (:double:) current tracking error (automatically becomes prev_e_y at next iteration).
+        e_int_y (:double:) current integral error (automatically becomes prev_int_y at next iteration).
     """
     
-    # error
-    e_y = y_ref - y_hat
-
-    # integral of the error
-    e_int_y = prev_int_y + e_y*delta_t  
+    # TODO: these are random values, you have to implement your own PID controller in here
+    omega = np.random.uniform(-8.0, 8.0)
+    e_y = np.random.random()
+    e_int_y = np.random.random()
     
-    # antiwindup
-    e_int_y = max(min(e_int_y,0.5),-0.5)
-
-    # derivative of the error
-    e_der_y = (e_y - prev_e_y)/delta_t
-
-    # PID parameters sim (v0 = 0.2, yref = 0.2 then -0.1)
-    Kp_y= 9
-    Ki_y= 0.02
-    Kd_y= 120 
-
-#     # PID parameters robot (v0=0.2, yref = 0.1 then -0.2)
-#     Kp_y= 9
-#     Ki_y= 0.05
-#     Kd_y= 12
-    
-    # PID controller for omega
-    omega = Kp_y*e_y + Ki_y*e_int_y + Kd_y*e_der_y
-    
-    u = [v_0, omega]
-    
-    # Debugging 
-    # print(f"\n\nDelta time : {delta_t} \nE : {e_y} \ne_int : {e_int_y} \ne_der : {e_der_y} \nU : {u} \ny_hat: {y_hat} \ny_ref: {y_ref}")
-
-    
-    return u, e_y, e_int_y
+    return [v_0, omega], e_y, e_int_y
 
--- mooc-modcon/solution/src/encoder_pose/src/odometry_activity.py
+++ mooc-modcon/solution/src/encoder_pose/src/odometry_activity.py
@@ -20,37 +20,9 @@ def DeltaPhi(encoder_msg, prev_ticks):
             ticks: current number of ticks (int)
     """
     
-#     # Read the number of ticks
-    
-#     ticks = None
-
-    
-#     # [...]
-    
-    
-#     delta_phi = None # in radians
-    
-    
-    
-    # Read the number of ticks
-    
-    ticks = encoder_msg.data
-
-    # Evaluate the number of ticks since the last call 
-    
-    delta_ticks = ticks-prev_ticks    
-
-    # Evaluate the wheel rotation
-
-    N_tot = encoder_msg.resolution #total number of ticks per wheel revolution
-
-    alpha = 2*np.pi/N_tot # rotation per tick in radians 
-
-    delta_phi = alpha*delta_ticks # in radians
-    
-    #     DEBUGGING
-    #     print(f"        DELTA TICKS  {encoder_msg.header.frame_id}")
-    #     print(delta_ticks)
+    # TODO: these are random values, you have to implement your own solution in here
+    ticks = prev_ticks + int(np.random.uniform(0, 10))     
+    delta_phi = np.random.random()
 
     return delta_phi, ticks
 
@@ -81,68 +53,10 @@ def poseEstimation( R, # radius of wheel (assumed identical) - this is fixed in 
             x_curr, y_curr, theta_curr (:double: values)
     """
     
-#     # Define wheel radii [m]
-    
-#     R = None
-    
-#     # Define distance travelled by each wheel [m]
-    
-#     d_left = None
-#     d_right = None
-    
-#     # Define distance travelled by the robot, in body frame [m]
-    
-#     d_A = None
-    
-#     # Define rotation of the robot [rad]
-    
-#     Dtheta = None
-    
-#     # Define distance travelled by the robot, in world frame [m]
-    
-#     Dx = None
-#     Dy = None
-    
-#     # Update pose estimate
-    
-#     x_curr = None
-#     y_curr = None
-#     theta_curr = None
-    
-    
-    
-    
-    
-    
-    # Define wheel radii [m]
-    
-    # r = 0 # make different than zero if you have reason to believe the wheels are of different sizes.
-    R_left = R # * (1-r)
-    R_right = R # * (1+r)
-    
-    # Define distance travelled by each wheel [m]
-    
-    d_left = R_left * delta_phi_left 
-    d_right = R_right * delta_phi_right
-    
-    # Define distance travelled by the robot, in body frame [m]
-    
-    d_A = (d_left + d_right)/2
-    
-    # Define rotation of the robot [rad]
-    
-    Dtheta = (d_right - d_left)/baseline_wheel2wheel
-    
-    # Define distance travelled by the robot, in world frame [m]
-    
-    Dx = d_A * np.cos(theta_prev)
-    Dy = d_A * np.sin(theta_prev)
-    
-    # Update pose estimate
-    
-    x_curr = x_prev + Dx
-    y_curr = y_prev + Dy
-    theta_curr = theta_prev + Dtheta
+    # TODO: these are random values, you have to implement your own solution in here
+    x_curr = np.random.random() 
+    y_curr = np.random.random() 
+    theta_curr = np.random.random() 
 
     return x_curr, y_curr, theta_curr
 
