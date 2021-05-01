7416549f1118e1a8a59bee197e965fd372cd76f2
Matthew R. Walter <mwalter@ttic.edu>
2021-05-01 16:12:57 -0500

    minor cleanup of notebooks; added icon

---
+++ visual_lane_servoing/assets/vnc/image/usr/bin/launch-vls-exercise.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sed -i "s/agent/${VEHICLE_NAME}/g" /opt/ros/noetic/share/rviz/odometry.rviz
+rviz -d /opt/ros/noetic/share/rviz/odometry.rviz &
+rostopic pub /$VEHICLE_NAME/activity_name std_msgs/String "data: pid_exercise"
