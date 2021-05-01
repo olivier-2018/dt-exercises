6e9474c2f6ecc961f048d58741e4626c52d5ece2
Matthew R. Walter <mwalter@ttic.edu>
2021-05-01 15:48:14 -0500

    added calibration exercise

--- visual_lane_servoing/launchers/default.sh
+++ visual_lane_servoing/launchers/default.sh
@@ -17,7 +17,7 @@ dt-launchfile-init
 
 dt-exec roslaunch --wait agent agent_node.launch
 dt-exec roslaunch --wait car_interface all.launch veh:=$VEHICLE_NAME
-dt-exec roslaunch encoder_pose encoder_pose_node.launch veh:=$VEHICLE_NAME
+dt-exec roslaunch visual_lane_servoing visual_lane_servoing_node.launch veh:=$VEHICLE_NAME
 
 # ----------------------------------------------------------------------------
 # YOUR CODE ABOVE THIS LINE
