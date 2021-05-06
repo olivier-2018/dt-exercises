8fddb22907ac24bbc2755d8194676a125e8adef3
Andrea F. Daniele <afdaniele@ttic.edu>
2021-05-06 12:27:01 -0500

    removed calibration icon from desktop; added joystick to launchers

--- visual_lane_following/assets/vnc/image/usr/bin/launch-vlf-exercise.sh
+++ visual_lane_following/assets/vnc/image/usr/bin/launch-vlf-exercise.sh
@@ -2,4 +2,5 @@
 sed -i "s/agent/${VEHICLE_NAME}/g" /opt/ros/noetic/share/rviz/viscontrol.rviz
 rviz -d /opt/ros/noetic/share/rviz/viscontrol.rviz &
 python3 /root/Documents/VLF-control-tool.py &
+dt-launcher-joystick &
 rostopic pub --latch "/${VEHICLE_NAME}/vlf_node/action" std_msgs/String "data: init"
