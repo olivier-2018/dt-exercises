f59d475a0831d8d39364df8ca23c352b804af5d4
Velythyl <charlie.gauthier@umontreal.ca>
2021-05-18 17:06:31 -0400

    filtering stuff

---
+++ mooc-object-detection/solution/src/object_detection/src/studentfile.py
@@ -0,0 +1,11 @@
+def NUMBER_FRAMES_SKIPPED():
+    return 0
+
+def filter_by_classes(classes):
+    return True
+
+def filter_by_bboxes(bboxes):
+    return True
+
+def filter_by_scores(scores):
+    return True
