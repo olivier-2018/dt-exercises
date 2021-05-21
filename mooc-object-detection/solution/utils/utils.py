57d2316f44d8694d230ebf967f1c9b05ec358127
liampaull <paulll@iro.umontreal.ca>
2021-05-21 09:21:11 -0400

    refactoring

--- mooc-object-detection/solution/utils/utils.py
+++ mooc-object-detection/solution/utils/utils.py
@@ -1,11 +1,62 @@
 import contextlib
-import os
-
 import numpy as np
 import random
 import math
 import os
+import subprocess
 
+
+def run(input, exception_on_failure=False):
+    try:
+        program_output = subprocess.check_output(f"{input}", shell=True, universal_newlines=True,
+                                                 stderr=subprocess.STDOUT)
+    except Exception as e:
+        if exception_on_failure:
+            raise e
+        program_output = e.output
+
+    return program_output
+
+
+def runp(input, exception_on_failure=False):
+    print(run(input, exception_on_failure))
+
+#make boxes to xywh format:
+def xminyminxmaxymax2xywfnormalized(box, image_size):
+    xmin, ymin, xmax, ymax = np.array(box, dtype=np.float64)
+    center_x = (xmin+xmax)/2
+    center_y = (ymin+ymax)/2
+    width = xmax-xmin
+    height = ymax-ymin
+
+    normalized = np.array([center_x, center_y, width, height])/image_size
+    return np.round(normalized, 5)
+
+def train_test_split(filenames, split_percentage, dataset_dir):
+    train_txt = np.array(filenames)
+    np.random.shuffle(train_txt)
+    nb_things = len(train_txt)
+    sp = int(split_percentage * nb_things)
+    train_txt, val_txt = train_txt[:sp], train_txt[sp:]
+
+    def mv(img_name, to_train):
+        dest = "train" if to_train else "val"
+        run(f"mv {dataset_dir}/images/{img_name}.jpg {dataset_dir}/{dest}/images/{img_name}.jpg")
+        run(f"mv {dataset_dir}/labels/{img_name}.txt {dataset_dir}/{dest}/labels/{img_name}.txt")
+
+    for img in train_txt:
+        mv(img, True)
+    for img in val_txt:
+        mv(img, False)
+
+
+@contextlib.contextmanager
+def makedirs(name):
+    try:
+        os.makedirs(name)
+    except:
+        pass
+    yield None
 
 @contextlib.contextmanager
 def directory(name):
@@ -14,7 +65,6 @@ def directory(name):
     yield None
     os.chdir(ret)
 
-@contextlib.contextmanager
 def makedirs(name):
     try:
         os.makedirs(name)
